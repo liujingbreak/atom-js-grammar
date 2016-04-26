f9a80181b8a6feea32267405f74879bcab407b18
Liu Jing <liujing.break@gmail.com>
2016-04-27 04:34:06 +0800

    wip

--- spec/sample01.js
+++ spec/sample01.js
@@ -1,23 +1,22 @@
-var obj = {
-	func1: function() {
-
-	},
-
-	func2: function(t) {
-
-	}
-};
-
-abc.efg = function func7() {};
-
-abc[1] = function func8() {};
-
-var func3 = function func4() {
-	return 'shit';
-};
-
-function func5() {
-	function func6() {
-
-	}
-}
+// var obj = {
+// 	func2: function(t) {
+// 		call(function() {});
+// 	}
+// };
+//
+// abc.efg = function func7() {};
+//
+a.b.c[1] = function func8() {};
+efg = function func9() {};
+// var func3 = function func4() {
+// 	return 'shit';
+// };
+//
+// function func5() {
+// 	function func6() {
+//
+// 	}
+// 	p.then(() => {
+// 		function func1() {}
+// 	});
+// }
